6f47dbb744c38fafe588c2a69adb05347bdddfad
yangchentech <yangchentech@163.com>
2021-10-26 14:26:42 +0800

    附件文件上传到券码审核列表

--- ucp-service/src/main/java/com/yum/ucp/modules/activity/http/NotifyInterfaceService.java
+++ ucp-service/src/main/java/com/yum/ucp/modules/activity/http/NotifyInterfaceService.java
@@ -3,19 +3,12 @@ package com.yum.ucp.modules.activity.http;
 import com.alibaba.fastjson.JSONObject;
 import com.yum.ucp.common.config.Global;
 import com.yum.ucp.common.utils.HttpClientUtil;
-import com.yum.ucp.modules.activity.dao.ActivityDao;
-import com.yum.ucp.modules.activity.entity.Activity;
-import com.yum.ucp.modules.activity.entity.NotifyActivity;
-import com.yum.ucp.modules.activity.entity.NotifyRefActivity;
-import com.yum.ucp.modules.activity.entity.QaReport;
-import com.yum.ucp.modules.activity.service.ActivityService;
-import com.yum.ucp.modules.activity.service.NotifyActivityService;
-import com.yum.ucp.modules.activity.service.NotifyRefActiviryService;
-import com.yum.ucp.modules.activity.service.QaReportService;
+import com.yum.ucp.common.utils.StringUtils;
+import com.yum.ucp.modules.activity.entity.*;
+import com.yum.ucp.modules.activity.service.*;
 import com.yum.ucp.modules.enumerate.NotifyActivityStatus;
 import com.yum.ucp.modules.sys.entity.Attach;
 import com.yum.ucp.modules.sys.service.AttachService;
-import com.yum.ucp.modules.sys.utils.ReadConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,12 +19,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class NotifyInterfaceService {
 
-    Logger logger=LoggerFactory.getLogger(this.getClass());
+    Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     NotifyActivityService notifyActivityService;
     @Autowired
@@ -39,25 +31,28 @@ public class NotifyInterfaceService {
     @Autowired
     AttachService attachService;
     @Autowired
+    UcpActFileService ucpActFileService;
+    @Autowired
     QaReportService qaReportService;
     @Autowired
     private ActivityService activityService;
 
 
-    private String notifySuccessUrl= Global.getConfig(Global.NOTIFY_SUCCESS_URL);
+    private String notifySuccessUrl = Global.getConfig(Global.NOTIFY_SUCCESS_URL);
 
 
     /**
      * 通知成功
+     *
      * @param activity
      */
-    public void notivifySuccess(Activity activity){
-        List<NotifyRefActivity> notifyRefActivityList =notifyRefActiviryService.findByActivityId(activity.getId());
-        if(!CollectionUtils.isEmpty(notifyRefActivityList)){
-            for (NotifyRefActivity notifyRefActivity:notifyRefActivityList){
-                NotifyActivity notifyActivity=notifyActivityService.get(notifyRefActivity.getNotifyId());
-                if(notifyActivity.getStatus().equals(NotifyActivityStatus.accept.getCode())){
-                    httpNotify(activity,notifyActivity);
+    public void notivifySuccess(Activity activity) {
+        List<NotifyRefActivity> notifyRefActivityList = notifyRefActiviryService.findByActivityId(activity.getId());
+        if (!CollectionUtils.isEmpty(notifyRefActivityList)) {
+            for (NotifyRefActivity notifyRefActivity : notifyRefActivityList) {
+                NotifyActivity notifyActivity = notifyActivityService.get(notifyRefActivity.getNotifyId());
+                if (notifyActivity.getStatus().equals(NotifyActivityStatus.accept.getCode())) {
+                    httpNotify(activity, notifyActivity);
                     break;
                 }
             }
@@ -66,51 +61,83 @@ public class NotifyInterfaceService {
     }
 
 
-
-
     /**
      * 请求通知
+     *
      * @param activity
      * @param notifyActivity
      */
     private void httpNotify(Activity activity, NotifyActivity notifyActivity) {
-        JSONObject req=new JSONObject();
-        Attach query=new Attach();
+        JSONObject req = new JSONObject();
+        Attach query = new Attach();
         query.setDelFlag("0");
         query.setClassPk(activity.getId());
-        req.put("activityId",notifyActivity.getActivityId());
+        req.put("activityId", notifyActivity.getActivityId());
 
-        List<Attach> list=attachService.findListByClassAndType(query);
+        List<Attach> list = attachService.findListByClassAndType(query);
+        List<UcpActFile> actFiles = ucpActFileService.findByActId(activity.getId());
 
-        QaReport qaReport =qaReportService.getByActId(activity.getId());
+        QaReport qaReport = qaReportService.getByActId(activity.getId());
 
-        if(qaReport!=null){
+        if (qaReport != null) {
             query.setClassPk(qaReport.getId());
-            List<Attach> list2=attachService.findListByClassAndType(query);
+            List<Attach> list2 = attachService.findListByClassAndType(query);
             list.addAll(list2);
         }
-        if(!CollectionUtils.isEmpty(list)) {
-            List<Map> maplist=new ArrayList<>();
-            for (Attach attach:list){
-                Map<String,Object> map=new HashMap<>();
-                map.put("filePath",attach.getFilePath());
-                map.put("fileName",attach.getFileName());
+        List<Map> maplist = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(list)) {
+            for (Attach attach : list) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("filePath", attach.getFilePath());
+                map.put("fileName", attach.getFileName());
                 maplist.add(map);
             }
-            req.put("fileList",maplist);
         }
+        if (!CollectionUtils.isEmpty(actFiles)) {
+            Map contentMap;
+            for (UcpActFile actFile : actFiles) {
+                contentMap = JSONObject.parseObject(actFile.getContent());
+                if (null != contentMap.get("attach")) {
+                    for (Map attach : (List<Map>) contentMap.get("attach")) {
+                        Map<String, Object> map = new HashMap<>();
+                        if (attach.containsKey("filePath")) {
+                            if (attach.get("filePath") == null || StringUtils.isBlank(attach.get("filePath").toString())) {
+                                continue;
+                            }
+                        }
+                        map.put("filePath", attach.get("filePath"));
+                        map.put("fileName", attach.get("fileName"));
+                        maplist.add(map);
+                    }
+                }
+                if (null != contentMap.get("images")) {
+                    for (Map attach : (List<Map>) contentMap.get("images")) {
+                        Map<String, Object> map = new HashMap<>();
+                        if (attach.containsKey("filePath")) {
+                            if (attach.get("filePath") == null || StringUtils.isBlank(attach.get("filePath").toString())) {
+                                continue;
+                            }
+                        }
+                        map.put("filePath", attach.get("filePath"));
+                        map.put("fileName", attach.get("fileName"));
+                        maplist.add(map);
+                    }
+                }
+            }
 
-        logger.info("http request---{},{}",notifySuccessUrl,req.toJSONString());
-        String respStr=HttpClientUtil.doPostJson(notifySuccessUrl,req.toJSONString());
-        logger.info("http response---{}",respStr);
-        JSONObject resp=JSONObject.parseObject(respStr);
-        if(resp.getInteger("code")==0){
-            String status=NotifyActivityStatus.getStatusByOutCode(resp.getString("status"));
-            notifyActivityService.updateStatusById(notifyActivity.getActivityId(),status);
-            logger.info("activityID :"+notifyActivity.getActivityId()+" was updated, status = "+status);
+        }
+        req.put("fileList", maplist);
+        logger.info("http request---{},{}", notifySuccessUrl, req.toJSONString());
+        String respStr = HttpClientUtil.doPostJson(notifySuccessUrl, req.toJSONString());
+        logger.info("http response---{}", respStr);
+        JSONObject resp = JSONObject.parseObject(respStr);
+        if (resp.getInteger("code") == 0) {
+            String status = NotifyActivityStatus.getStatusByOutCode(resp.getString("status"));
+            notifyActivityService.updateStatusById(notifyActivity.getActivityId(), status);
+            logger.info("activityID :" + notifyActivity.getActivityId() + " was updated, status = " + status);
             return;
-        }else{
-            logger.error("notify error code--{}",resp.getInteger("code"));
+        } else {
+            logger.error("notify error code--{}", resp.getInteger("code"));
         }
     }
 
